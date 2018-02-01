f871c3b27c2d237c87d7afd2b3433569da07f3ef
Kamal <kamal@dashinggroup.com.au>
2018-02-01 14:52:07 +1100

    initial

---
+++ Dashing.Dam/Controllers/DamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dashing.Dam.Controllers
+{
+    public class DamController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetFolders()
+        {
+            return Ok(12345647);
+        }
+    }
+}
